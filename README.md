aff5b648128f91df45c2f50781066c61aa6dfab2
Mike Ferg <mikeferguson@designprollc.com>
2024-05-27 11:10:43 -0400

    added files

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# professional-readme-generator
+# undefined
+
+## Description
+
+pro read me generator
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Reporting Issues](#reporting-issues)
+- [Acknowledgments](#Acknowledgments)
+
+## Installation
+
+undefined
+
+## Usage
+
+undefined
+
+## Reporting Issues
+
+no
+
+## Acknowledgments
+
+me
+
+video file can be found at professional-readme-generator\assets\video.webm
--- index.js
+++ index.js
@@ -1,13 +1,79 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "description",
+    message: "What is the name of this application?",
+  },
+  {
+    type: "input",
+    name: "Usage",
+    message: "Describe what this application does?",
+  },
+  {
+    type: "input",
+    name: "Installation",
+    message: "How to install this application?",
+  },
+  {
+    type: "input",
+    name: "issues",
+    message: "Are there any known issues?",
+  },
+  {
+    type: "input",
+    name: "acknowledgments",
+    message: "Who would you like to credit?",
+  },
+
+  //   "Description",
+  //   "Table of Contents",
+  //   "Installation",
+  //   "Usage",
+  //   "Reporting Issues",
+  //   "Acknowledgments",
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log("README.md created successfully!")
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const readmeContent = `# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Reporting Issues](#reporting-issues)
+- [Acknowledgments](#Acknowledgments)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Reporting Issues
+${answers.issues}
+
+## Acknowledgments
+${answers.acknowledgments}
+`;
+
+    writeToFile("README.md", readmeContent);
+  });
+}
 
 // Function call to initialize app
 init();
